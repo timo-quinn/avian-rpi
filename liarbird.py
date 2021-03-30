609101411aea839f4e04f77477009df94aac592c
Tim <tim@congruentlabs.co>
2021-03-31 02:43:49 +1100

    reading and writing config properly

--- liarbird.py
+++ liarbird.py
@@ -14,7 +14,8 @@ config.read('config.ini')
 
 auth_key = config['general'].get('auth_key')
 device_uid = config['general'].get('device_uid')
-interval = config['device'].get('interval')
+force_playback_only = config['general'].getboolean('force_playback_only')
+interval = config['device'].getint('interval')
 sample_file = config['device'].get('sampleFile')
 
 logging.basicConfig(
@@ -23,6 +24,10 @@ logging.basicConfig(
     format='%(asctime)s %(levelname)-6s %(lineno)d %(name)-6s %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S')
 
+logging.debug(auth_key)
+logging.debug(device_uid)
+logging.debug(interval)
+logging.debug(sample_file)
 
 if __name__ == '__main__':
   internet_connected = True
@@ -33,15 +38,16 @@ if __name__ == '__main__':
     internet_connected = False
 
   try:
-    if internet_connected:
+    if internet_connected and not force_playback_only:
       logging.info("internet connection found. running in configuration mode")
 
       if not device_uid:
         logging.info('no device identifier set - registering device')
-        response = requests.post("https://us-central1-liarbird-1df1e.cloudfunctions.net/registerDevice", data={ 'authKey': auth_key })
+        response = requests.post("https://us-central1-liarbird-1df1e.cloudfunctions.net/registerDevice", data={ "authKey": auth_key })
         
         if response.status_code != 200:
           logging.error(response)
+
         else:
           logging.debug(response.text)
           json_response = json.loads(response.text)
@@ -49,27 +55,35 @@ if __name__ == '__main__':
           device_uid = json_response['uid']
 
           logging.info('updating config.ini')
-          config.write(open('config.ini', 'wb'))
+          config.write(open('config.ini', 'w'))
 
-      logging.info('fetching config')
-      response = requests.post("https://us-central1-liarbird-1df1e.cloudfunctions.net/getConfiguration", data={ 'authKey': auth_key, 'uid': device_uid })
+      if device_uid:
+        logging.info('fetching config')
+        response = requests.post("https://us-central1-liarbird-1df1e.cloudfunctions.net/getConfiguration", data={ "authKey": auth_key, "uid": device_uid })
 
-      if response.status_code != 200:
-        # failed request
-        logging.error(response)
-      else:
-        logging.info('config retrieved from server')
-        logging.debug(response.text)
-        response_data = json.loads(response.text)
-        
-        config.set('device', 'interval', response_data['playbackFrequency'])
-        config.set('device', 'sampleFile', response_data['sampleFile'])
+        if response.status_code != 200:
+          # failed request
+          logging.error(response)
 
-        logging.info('fetching sample')
-        response = requests.get(response_data["sampleUri"])
+        else:
+          logging.info('config retrieved from server')
+          logging.debug(response.text)
+          response_data = json.loads(response.text)
 
-        logging.info('writing sample to disk')
-        open(response_data["sampleFile"], 'wb').write(response.content)
+          if 'playbackFrequency' in response_data:
+            config.set('device', 'interval', response_data['playbackFrequency'])
+            config.write(open('config.ini', 'w'))
+          if 'sampleFile' in response_data:
+            config.set('device', 'sampleFile', response_data['sampleFile'])
+            config.write(open('config.ini', 'w'))
+          if 'sampleUri' in response_data:
+            logging.info('fetching sample')
+            response = requests.get(response_data["sampleUri"])
+
+            config.write(open('config.ini', 'w'))
+
+            logging.info('writing sample to disk')
+            open(response_data["sampleFile"], 'wb').write(response.content)
 
     else:
       logging.info("NO internet connection found. running in playback mode")
@@ -80,7 +94,9 @@ if __name__ == '__main__':
         logging.error("missing interval!")
       else:
         logging.info("running as normal")
+        pygame.mixer.init()
         while True:
+          logging.info("starting playback of sample_file")
           pygame.mixer.music.load(sample_file)
           pygame.mixer.music.play()
           
